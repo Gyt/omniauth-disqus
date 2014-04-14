11541893a0e124637a4558accb6614f9fe18bee3
Gyt <noibin@gmail.com>
2014-04-14 17:59:03 +0400

    'bug-fixes'

--- lib/omniauth/strategies/disqus.rb
+++ lib/omniauth/strategies/disqus.rb
@@ -1,6 +1,5 @@
 require 'omniauth/strategies/oauth2'
-require 'multi_json'
-require 'httparty'
+require 'open-uri'
 
 module OmniAuth
   module Strategies
@@ -11,15 +10,16 @@ module OmniAuth
         :authorize_url => '/api/oauth/2.0/authorize/?scope=read,email',
         :token_url      => '/api/oauth/2.0/access_token/'
         }
-      uid { access_token.params['user_id'] }
+      uid { access_token.params['user_id'].to_s }
 
       info do
         {
           :name => raw_info['name'],
+          :email => raw_info['email'],
           :nickname => raw_info['username'],
           :urls => { 'profileUrl' => raw_info['profileUrl'] },
           :location => raw_info['location'],
-          :image => raw_info['avatar']['small']['permalink'],
+          :image => raw_info['avatar']['small']['permalink']
         }
       end
 
@@ -29,7 +29,7 @@ module OmniAuth
       
       def raw_info
         begin
-          res = HTTParty.get("http://disqus.com/api/3.0/users/details.json?access_token=#{access_token.token}&api_key=#{access_token.client.id}}")
+          res = JSON.parse(open("https://disqus.com/api/3.0/users/details.json?access_token=#{access_token.token}&api_key=#{access_token.client.id}").read)
         rescue => e
           e # what should be done with this?
         end
